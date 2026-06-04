9ccb2c952fe3b00aa5c880da1391bbe5f9cfc527
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-06-04 15:56:08 +0200

    Aggiunta soluzione 3

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -7,3 +7,9 @@ WHERE YEAR(date_of_birth) = 1990;
 SELECT *
 FROM courses
 WHERE cfu > 10;
+
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT *
+FROM students
+WHERE TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) > 30;
+
